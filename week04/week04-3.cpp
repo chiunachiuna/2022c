f0ae5933928528c430c4e918802b56446cfc85c2
chiunachiuna <09050053@me.mcu.edu.tw>
2023-03-10 11:42:17 +0800

    Week04

---
+++ week04/week04-3.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=10,b=20;
+
+    int *p1=&a;
+    int *p2=&b;
+
+    printf("p1指標的變數值是%d\n",*p1);
+    printf("p2指標的變數值是%d\n",*p2);
+
+    p1=p2;
+    printf("p1指到的變數值是%d\n",*p1);
+    printf("p2指到的變數值是%d\n",*p2);
+
+}
